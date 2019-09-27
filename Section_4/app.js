5e92e80725636570173fe07b48250a4492631c1a
Longoria.You <longoria.you@gmail.com>
2019-09-27 16:00:36 +0800

    Argument Parsing with Yargs: Part II

--- Section_4/app.js
+++ Section_4/app.js
@@ -9,28 +9,40 @@ const note = require('./note');
 //console.log(yargs.argv.title);
 
 // chalk samples
-console.log(chalk.blue('Success!'));
+//console.log(chalk.blue('Success!'));
 //console.log(chalk.red.bold.bgWhite.bold('red with white background!'));
 //console.log(chalk.blue.bgRed('Hello world!'));
 
 //Add Command
-yargs.command('add', 'Add a new note', function () {
-    console.log('add a new note!');
+yargs.command('add', 'Add a new note', function (yargs) {
+    return yargs.option('title', {
+        describe: 'Note Title',
+        demandOption: true,        
+        type: 'string'
+    }).option('body', {
+        describe: 'Note Body',
+        demandOption: true,
+        type: 'string'
+    });
+}, function (argv) {
+    //console.log('add a new note!', argv);
+    console.log(`Title: ${argv.title}\nBody: ${argv.body}`);
 });
 
 // Remove Command
 yargs.command('remove', 'Remove a note', function () {
     console.log('remove one note!');
-})
+});
 
 // Read Command
 yargs.command('read', 'Read a note', function () {
     console.log('read one note!');
-})
+});
 
 // List Command
 yargs.command('list', 'List out all the notes', function () {
     console.log('list out all the notes!');
-}).help().argv;
+});
 
-console.log(yargs.argv);
+yargs.argv;
+//console.log(yargs.argv);
--- Section_4/note.js
+++ Section_4/note.js
@@ -3,14 +3,39 @@ const fs = require('fs');
 //fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
 //fs.appendFileSync('notes.txt', '\nAppend some text viw "appendFileSync" method');
 
-const fileName = 'notes.txt';
+const fileName = 'notes.json';
 
-function append(msg){
+function getNotes() {
+    const dataJson = fs.readFileSync(fileName, 'utf8');
+    return JSON.parse(dataJson);
+}
+
+function loadNotes() {
+    try {
+        const dataBuffer = fs.readFileSync(fileName);
+        const dataJson = dataBuffer.toString();
+        return JSON.parse(dataJson);
+    } catch (e) {
+        return [];
+    }
+}
+
+function append(msg) {
     fs.appendFile(fileName, msg);
 }
 
-function getNotes(){
-    return fs.readFileSync(fileName, 'utf8');
+function addNote(title, body) {
+    const notes = loadNotes();
+    notes.push({
+        title,
+        body
+    });
+    saveNotes(notes);
 }
 
-module.exports = { name: fileName, append, getNotes };
+function saveNotes(notes){
+    const dataJSON = json.stringify(notes);
+    fs.writeFileSync(fileName, dataJSON);
+}
+
+module.exports = { append, getNotes };
